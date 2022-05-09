3902cd97a5b866bbf3257e5ce971e4c63b711e74
Felipe Arturo Glz <arturoglezggg@gmail.com>
2022-05-08 22:24:07 -0500

    Gifs module and page added

---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GifsPageComponent } from './gifs-page/gifs-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    GifsPageComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    GifsPageComponent
+  ]
+})
+export class GifsModule { }
