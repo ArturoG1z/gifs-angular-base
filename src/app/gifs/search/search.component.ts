fdbaf6b43d4ca2bb40efbeb0ccd126a1594b095b
Felipe Arturo Glz <arturoglezggg@gmail.com>
2022-05-08 22:39:35 -0500

    Search view child added for input search

--- src/app/gifs/search/search.component.ts
+++ src/app/gifs/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
+  @ViewChild('txtSearch') txtSearch!: ElementRef<HTMLInputElement>;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  search(term : string) {
+    const value = this.txtSearch.nativeElement.value;
+    console.log(value);
+    this.txtSearch.nativeElement.value = '';
+  }
 }
