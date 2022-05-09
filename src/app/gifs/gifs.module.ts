7d8b459247ea5abff9688d60b1f330051de1643c
Felipe Arturo Glz <arturoglezggg@gmail.com>
2022-05-08 22:26:04 -0500

    Search and gifs added

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GifsPageComponent } from './gifs-page/gifs-page.component';
+import { SearchComponent } from './search/search.component';
+import { ResultsComponent } from './results/results.component';
 
 
 
 @NgModule({
   declarations: [
-    GifsPageComponent
+    GifsPageComponent,
+    SearchComponent,
+    ResultsComponent
   ],
   imports: [
     CommonModule
